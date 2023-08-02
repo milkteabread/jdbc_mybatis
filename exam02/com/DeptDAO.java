0178927b423162b144f9c58ae3e94036a00cb9be
김문정 <98167564+milkteabread@users.noreply.github.com>
2023-08-02 18:23:27 +0900

    DAO, DTO, Exception, Service

---
+++ exam02/com/DeptDAO.java
@@ -0,0 +1,110 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dto.DeptDTO;
+import com.exception.DuplicatedDeptnoException;
+
+// Oracle의 dept테이블 연동하는 클래스
+public class DeptDAO {
+
+	// select 기능하는 메서드
+	public List<DeptDTO> findAll(Connection con) {
+		// DeptDTO 누적용
+		List<DeptDTO> list = new ArrayList<DeptDTO>();// 다형성
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "select deptno as no, dname, loc from dept";
+			pstmt = con.prepareStatement(sql); // 에러발생시
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				int deptno = rs.getInt("no"); // getInt(1) 가능
+				String dname = rs.getString("dname");
+				String loc = rs.getString("loc");
+				DeptDTO dto = new DeptDTO(deptno, dname, loc);
+				// 누적
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 역순
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}//
+
+	// insert 작업
+	public int insert(Connection con, DeptDTO dto) throws DuplicatedDeptnoException {
+		int num = 0;
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "insert into dept ( deptno, dname, loc) " + " values( ?, ?, ? )";
+			pstmt = con.prepareStatement(sql); // 에러발생시
+			pstmt.setInt(1, dto.getDeptno()); // deptno 값은 중복되지 않도록 확인할 것.
+			pstmt.setString(2, dto.getDname());
+			pstmt.setString(3, dto.getLoc());
+			num = pstmt.executeUpdate();
+		} catch (SQLException e) {
+//	    	e.printStackTrace();
+			throw new DuplicatedDeptnoException(dto.getDeptno() + " 값이 중복됨");
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return num;
+	}
+
+	public int update(Connection con, DeptDTO dto) {
+		int num = 0;
+		PreparedStatement pstmt=null;
+	    try{
+	    	String sql = "update dept set dname=?, loc=? where deptno=?";
+	       pstmt = con.prepareStatement(sql); 
+	       pstmt.setInt(3,dto.getDeptno());
+	       pstmt.setString(1,dto.getDname());
+	       pstmt.setString(2, dto.getLoc());  
+	       num = pstmt.executeUpdate();
+	       
+	    }catch(SQLException e){
+	    	e.printStackTrace();
+	    }finally {
+			try {
+				if(pstmt != null)pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	    }
+		return num;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ exam02/com/DeptServiceimpl.java
@@ -0,0 +1,110 @@
+package com.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.dao.DeptDAO;
+import com.dto.DeptDTO;
+import com.exception.DuplicatedDeptnoException;
+
+public class DeptServiceimpl implements DeptService {
+
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String userid = "SCOTT";
+	String passwd = "TIGER";
+
+	public DeptServiceimpl() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// select 기능하는 메서드
+	// Connection 까지만 얻는다. 나머지 작업은 DAO에서 처리
+	@Override
+	public List<DeptDTO> findAll() {
+
+		List<DeptDTO> list = null;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			// DAO 접근
+			DeptDAO dao = new DeptDAO();
+			list = dao.findAll(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	// insert 기능하는 메서드
+    // Connection 까지만 얻는다. 나머지 작업은 DAO에서 처리
+	@Override
+	public int insert(DeptDTO dto) throws DuplicatedDeptnoException {
+		int n = 0;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+	        // DAO 연동
+			DeptDAO dao = new DeptDAO();
+			n = dao.insert(con, dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return n;
+	}
+
+	// update 기능하는 메서드
+    // Connection 까지만 얻는다. 나머지 작업은 DAO에서 처리
+	@Override
+	public int update(DeptDTO dto) {
+		int n = 0;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			//DAO 연동
+			DeptDAO dao = new DeptDAO();
+			n = dao.update(con, dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return n;
+	}
+
+
+}
+
+
+
+
