361a106d6b627e858d2217ba0c75d47b426bf1fc
김문정 <98167564+milkteabread@users.noreply.github.com>
2023-08-04 17:11:10 +0900

    mybatis_update

---
+++ exam03/MybatisStudy3/DeptDTO.java
@@ -0,0 +1,53 @@
+package com.dto;
+
+
+//dept 테이블의 하나의 행(레코드)을 저장하는 용도
+public class DeptDTO {
+	
+	int deptno;  //dept 테이블의 deptno 컬럼 저장
+	String dname;  //dept 테이블의 dname 컬럼 저장
+	String loc;  //dept 테이블의 loc 컬럼 저장
+	
+	
+    public DeptDTO(int deptno, String dname, String loc) {
+		this.deptno = deptno;
+		this.dname = dname;
+		this.loc = loc;
+	}
+
+	public DeptDTO() {
+		
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	@Override
+	public String toString() {
+		return deptno +"\t"+ dname +"\t" + loc +"\t";
+	}
+	
+    
+
+}
---
+++ exam03/MybatisStudy3/DeptMain2.java
@@ -0,0 +1,51 @@
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.dto.DeptDTO;
+
+public class DeptMain2 {
+
+	public static void main(String[] args) throws Exception {
+		// Configuration.xml 파일 읽기
+		
+		String resource = "com/config/Configuration.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory =
+		new SqlSessionFactoryBuilder().build(inputStream);
+		
+		SqlSession session = sqlSessionFactory.openSession();
+			
+		//insert 작업
+//		DeptDTO dto = new DeptDTO(71,"개발","서울");
+//		int n = session.insert("DeptMapper2.addDept", dto);
+//		System.out.println(n+ "개가 저장됨");
+//		session.commit(); //트랜잭션 종료
+		
+		//update 작업
+//		HashMap<String, Object> map = new HashMap<>();
+//		map.put("deptno", 71);
+//		map.put("dname", "개발부");
+//		map.put("loc", "서울시");
+		
+//		int n2 = session.update("DeptMapper2.updateDept", map);
+//		System.out.println(n2+ "개가 수정됨");
+//		session.commit();
+		
+		//delete 작업
+		int n3 = session.delete("DeptMapper2.updateDept", 71);
+		System.out.println(n3+ "개가 삭제됨");
+		session.commit();
+		
+		session.close();
+
+	}
+
+}
